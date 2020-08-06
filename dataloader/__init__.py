044fb8f75ee31fda49cf88e3cad0c226472097a2
jfwu777 <jfwu.ai@gmail.com>
2020-08-07 02:02:32 +0800

    update

--- dataloader/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-"""
-Author: Junfeng Wu (junfeng.wu@ghddi.org)
-TODO: write a README for all data_loader
-"""
-
-from .data_loader import *
--- model/__init__.py
+++
@@ -1,6 +0,0 @@
-# coding: utf-8
-"""
-Author: Junfeng Wu (junfeng.wu@ghddi.org)
-"""
-
-from .model import model_4v1
